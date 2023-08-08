52bc4d9c11ff7b093c134e3ecc59f949271017fd
Antonio_Chacon <antonio_chacon@epam.com>
2023-08-08 12:34:12 +0200

    public class GreetingService implements ApplicationListener { Issue fixing attempt. correction, created in GreetingService

--- src/main/java/com/demo/service/GreetingService.java
+++ src/main/java/com/demo/service/GreetingService.java
@@ -1,26 +1,17 @@
 package com.demo.service;
 
-import org.springframework.context.ApplicationEvent;
-import org.springframework.context.ApplicationListener;
-import org.springframework.stereotype.Component;
-
-@Component
-public class GreetingService implements ApplicationListener {
+public class GreetingService {
 
     private final String greeting;
 
-    public GreetingService(String greeting){
+    public GreetingService(String greeting) {
         super();
         this.greeting = greeting;
     }
 
-    public String getGreeting(String greeting){
+    public String getGreeting(String greeting) {
         System.out.println(greeting);
         return greeting;
     }
 
-    @Override
-    public void onApplicationEvent(ApplicationEvent event) {
-
-    }
 }
