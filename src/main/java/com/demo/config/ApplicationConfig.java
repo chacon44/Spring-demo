715df7a616913c7704455ece41a51a5061f4517c
Antonio_Chacon <antonio_chacon@epam.com>
2023-08-10 17:07:03 +0200

    Error 404 solved adding ComponentScan

--- src/main/java/com/demo/config/ApplicationConfig.java
+++ src/main/java/com/demo/config/ApplicationConfig.java
@@ -4,9 +4,10 @@ import com.demo.service.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
+@ComponentScan(basePackages = "com.demo")
 @Configuration
 @EnableWebMvc
 public class ApplicationConfig {
