b505794e4039f214e31bc8f137700200098a5227
Antonio_Chacon <antonio_chacon@epam.com>
2023-09-05 13:47:12 +0200

    Stuck in subtask6

---
+++ src/main/java/com/demo/exceptions/ApiError.java
@@ -0,0 +1,27 @@
+package com.demo.exceptions;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiError extends RuntimeException{
+
+    private HttpStatus status;
+    private String message;
+    private List<String> errors;
+
+    public ApiError(HttpStatus status, String message, List<String> errors) {
+        super();
+        this.status = status;
+        this.message = message;
+        this.errors = errors;
+    }
+
+    public ApiError(HttpStatus status, String message, String error) {
+        super();
+        this.status = status;
+        this.message = message;
+        errors = Arrays.asList(error);
+    }
+}
--- src/main/java/com/demo/exceptions/ControllerAdviceClass.java
+++ src/main/java/com/demo/exceptions/ControllerAdviceClass.java
@@ -1,15 +1,37 @@
 package com.demo.exceptions;
 
 import com.demo.dto.ErrorResponseDTO;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.Objects;
+
 @ControllerAdvice("com.demo.controllers")
 public class ControllerAdviceClass extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ApiError.class)
+    @Override
+    protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+            HttpRequestMethodNotSupportedException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(ex.getMethod());
+        builder.append(
+                " method is not supported for this request. Supported methods are ");
+        Objects.requireNonNull(ex.getSupportedHttpMethods()).forEach(t -> builder.append(t).append(" "));
+
+        ApiError apiError = new ApiError(HttpStatus.METHOD_NOT_ALLOWED,
+                ex.getLocalizedMessage(), builder.toString());
+        return new ResponseEntity<>(apiError, new HttpHeaders(), HttpStatus.METHOD_NOT_ALLOWED);
+    }
     @ExceptionHandler(OutOfIdException.class)
     public ResponseEntity<Object> handleMyException(OutOfIdException ex, WebRequest request) throws Exception {
         if (ex.getCode() == ErrorCode.OUT_OF_IDS) {
