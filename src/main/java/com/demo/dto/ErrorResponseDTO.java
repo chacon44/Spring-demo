66c015dae5e17b2d1bbfa4a6f7096fce574fd794
Antonio_Chacon <antonio_chacon@epam.com>
2023-09-05 15:29:40 +0200

    Subtask 6 first version

--- src/main/java/com/demo/dto/ErrorResponseDTO.java
+++ src/main/java/com/demo/dto/ErrorResponseDTO.java
@@ -2,5 +2,5 @@ package com.demo.dto;
 
 import com.demo.exceptions.ErrorCodeEnums;
 
-public record ErrorResponseDTO (ErrorCodeEnums errorCodeEnums, String description) {
+public record ErrorResponseDTO (ErrorCodeEnums error, String description) {
 }
