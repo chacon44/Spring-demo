70ededa582f1dec4f7998fce4259500b8e3c27fd
Antonio_Chacon <antonio_chacon@epam.com>
2023-09-28 12:24:33 +0200

    Subtask 10

---
+++ src/main/java/com/demo/database/Database.java
@@ -0,0 +1,23 @@
+package com.demo.database;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@ComponentScan("com.demo.database")
+public class Database {
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:5433/database");
+        dataSource.setUsername("postgre");
+        dataSource.setPassword("1");
+
+        return dataSource;
+    }
+}
